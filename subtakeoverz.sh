45ec48d796c1766bf262230f7eb8371f01a49579
Surya <65324191+bughuntersurya@users.noreply.github.com>
2021-07-17 14:32:42 +0530

    Add files via upload

---
+++ subtakeoverz.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+if [ ! -d "subtakeoverz" ];then
+	mkdir subtakeoverz
+fi
+
+while :
+do
+echo "subtakeoverz Started" | notify
+subfinder -silent -dL $1 | anew ~/subtakeoverz/subs.txt
+echo "Total Subdomains" | notify
+cat ~/subtakeoverz/subs.txt | wc -l | notify
+sleep 100
+cat ~/subtakeoverz/subs.txt | httpx -threads 30 | tee ~/subtakeoverz/live.txt
+echo "Total Livedomains" | notify
+sleep 5
+cat ~/subtakeoverz/live.txt | wc -l | notify
+sleep 5
+echo "Takeover Scan Started" | notify
+nuclei -update-templates
+echo "Templates updated" | notify
+cat ~/subtakeoverz/live.txt | nuclei -rl 100 -t /root/nuclei-templates -tags takeover | tee ~/subtakeoverz/nuclei.txt | notify
+echo "Takeover Scan Finished" | notify
+echo "subtakeoverz Finished" | notify
+sleep 3600
+done
