b823f6360ed03d4a7f1232dce4bd259ca54703e1
Surya <65324191+bughuntersurya@users.noreply.github.com>
2021-09-19 11:17:03 +0530

    Update subtakeoverz.sh

--- subtakeoverz.sh
+++ subtakeoverz.sh
@@ -4,22 +4,20 @@ if [ ! -d "subtakeoverz" ];then
 	mkdir subtakeoverz
 fi
 
-while :
-do
 echo "subtakeoverz Started" | notify
 subfinder -silent -dL $1 | anew ~/subtakeoverz/subs.txt
 echo "Total Subdomains" | notify
 cat ~/subtakeoverz/subs.txt | wc -l | notify
-sleep 100
+sleep 60
 cat ~/subtakeoverz/subs.txt | httpx -threads 30 | tee ~/subtakeoverz/live.txt
 echo "Total Livedomains" | notify
 sleep 5
 cat ~/subtakeoverz/live.txt | wc -l | notify
 sleep 5
+while :
+do
 echo "Takeover Scan Started" | notify
-nuclei -update-templates
-echo "Templates updated" | notify
-cat ~/subtakeoverz/live.txt | nuclei -rl 100 -t /root/nuclei-templates -tags takeover | tee ~/subtakeoverz/nuclei.txt | notify
+cat ~/subtakeoverz/live.txt | nuclei -rl 100 -c 50 -t /root/nuclei-templates -tags takeover | tee ~/subtakeoverz/nuclei.txt | notify
 echo "Takeover Scan Finished" | notify
 echo "subtakeoverz Finished" | notify
 sleep 3600
